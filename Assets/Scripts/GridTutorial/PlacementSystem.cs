cda692d197885e73f5b2690b9cc47d87a517b16f
Boyd van Lingen <boydvanlingen@gmail.com>
2025-01-15 16:03:12 +0100

    Improved Tower Building
    you can now place multiple towerr
    towers can not be placed in the same tile
    A preview of the tower that is about to be placed is shown.
    The preview indicates if the tower can be placed or not by changing the previews color

--- Assets/Scripts/GridTutorial/PlacementSystem.cs
+++ Assets/Scripts/GridTutorial/PlacementSystem.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour {
@@ -5,64 +7,94 @@ public class PlacementSystem : MonoBehaviour {
     public TowerLocations TowerLocations;
 
     [SerializeField]
-    GameObject cellIndicator;
-    [SerializeField]
     InputManager inputManager;
     [SerializeField]
     private Grid grid;
 
     [SerializeField]
     private ObjectDatabase database;
-    private int selectedObjectIndex = -1;
 
     [SerializeField]
     private GameObject gridVisualisation;
 
+    private GridData floorData;
+
+
+    [SerializeField]
+    private PreviewSystem preview;
+
+    private Vector3Int lastDetectedPosition = Vector3Int.zero;
+
+    [SerializeField]
+    private ObjectPlacer objectPlacer;
+
+    IBuildingState buildingState;
     private void Start() {
         StopPlacement();
+        floorData = new GridData();
     }
 
     public void StartPlacement(int id) {
-        selectedObjectIndex = database.Towers.FindIndex(data => data.Id == id);
+        StopPlacement();
 
-        if(selectedObjectIndex < 0) {
-            Debug.LogError($"Cant find index in database id: {id}");
-            return;
-        }
         gridVisualisation.SetActive(true);
-        cellIndicator.SetActive(true);
+
+        buildingState = new PlacementState(id,
+                                           grid,
+                                           preview,
+                                           database,
+                                           floorData,
+                                           objectPlacer);
+
         inputManager.OnClicked += PlaceStructure;
         inputManager.OnExit += StopPlacement;
     }
 
-    private void PlaceStructure() {
-        if (inputManager.IsPointerOverUI()) {
+    public void StartRemoving()
+    {
+        StopPlacement();
+        gridVisualisation.SetActive(true);
+        buildingState = new RemovingState(grid, preview, floorData, objectPlacer);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+    private void PlaceStructure() 
+    {
+        if (inputManager.IsPointerOverUI()) 
+        {
             return;
         }
-
-        GameObject towerToPlace = database.Towers[selectedObjectIndex].Prefab;
-
-        TowerLocations.AddTowerLocation(towerToPlace);
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridposition = grid.WorldToCell(mousePosition);
-        GameObject newGameobject = Instantiate(towerToPlace);
-        newGameobject.transform.position = grid.CellToWorld(gridposition);
+
+        buildingState.OnAction(gridposition);
     }
 
     private void StopPlacement() {
+        if(buildingState == null)
+        {
+            return;
+        }
         gridVisualisation.SetActive(false);
-        cellIndicator.SetActive(false);
+        buildingState.EndState();
         inputManager.OnClicked -= PlaceStructure;
         inputManager.OnExit -= StopPlacement;
+        lastDetectedPosition = Vector3Int.zero;
+        buildingState = null;
     }
 
     private void Update() {
-        if (selectedObjectIndex < 0) {
+        if (buildingState == null) {
             return;
         }
 
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
-        Vector3Int gridposition = grid.WorldToCell(mousePosition);
-        cellIndicator.transform.position = grid.CellToWorld(gridposition);
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        if(lastDetectedPosition != gridPosition)
+        {
+            buildingState.UpdateState(gridPosition);
+            lastDetectedPosition = gridPosition;
+        }
     }
 }
