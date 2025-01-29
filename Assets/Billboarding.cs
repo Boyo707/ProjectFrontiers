67ad5a4c21e7d2832f3f1f9e68b3bdd66a0488cf
Boyd van Lingen <boydvanlingen@gmail.com>
2025-01-29 13:33:12 +0100

    Enemy and Projectile Visuals added

--- Assets/Billboarding.cs
+++ Assets/Billboarding.cs
@@ -5,7 +5,14 @@ public class Billboarding : MonoBehaviour
     //12 hours of work
     private void LateUpdate()
     {
-        transform.LookAt(Camera.main.transform);
+        if (transform.parent != null)
+        {
+            transform.LookAt(Camera.main.transform, transform.parent.transform.forward);
+        }
+        else
+        {
+            transform.LookAt(Camera.main.transform, transform.forward);
+        }
         transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, 180, 0);
     }
 }
